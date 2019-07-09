ae4e9fa6a7ec9eb34c5c97979eed30a508e1917c
Mahdi Sadeghzadeh Ghamsary <mahdisadeghzadeh24@gmail.com>
2019-07-10 02:40:05 +0430

    database changed to postgresql

--- template_event_site/template_event_site/settings.py
+++ template_event_site/template_event_site/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'template_event_site.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": 'ai_database',
+        "USER": 'event_owner',
+        "PASSWORD": 'anjoman',
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
