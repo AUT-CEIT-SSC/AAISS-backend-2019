7caa914a754b9e44cdbbed59e63f089f6afdf40a
MSadeghzadehG <mahdisadeghzadeh24@gmail.com>
2019-07-12 21:10:17 +0430

    id added

--- template_event_site/ai_talks/serializers.py
+++ template_event_site/ai_talks/serializers.py
@@ -5,5 +5,5 @@ from rest_framework import serializers
 class SpeakerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Speaker
-        fields = '__all__'
-	#fields = ['name','position','bio','abstract','image_path','date_and_time','talk_location','talk_title','id']
+        # fields = '__all__'
+        fields = ('name','position','bio','abstract','image_path','date_and_time','talk_location','talk_title','id')
